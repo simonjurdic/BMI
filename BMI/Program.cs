6ae68819162c0c4871093d86a8ca405fafde02b9
Šimon Jurdič <Jurdic.Simon@stud.vassboskovice.cz>
2021-10-19 09:40:21 +0200

    XCVBNM

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,7 +6,39 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double weight;
+            double height;
+            double BMI;
+
+
+            Console.WriteLine("BMI kalkulačka");
+            Console.WriteLine("zadejte váhu v kilogramech");
+           weight=double.Parse (Console.ReadLine());
+            Console.WriteLine("zadejte výšku v metrech");
+            height=double.Parse (Console.ReadLine());
+
+            BMI = weight / (height * height);
+            Console.WriteLine();
+            BMI = Math.Round(BMI,1);
+            Console.WriteLine(BMI);
+
+
+            if (BMI < (18.5))
+            {
+                Console.WriteLine("podváha");
+
+            }
+
+            if (BMI < ())
+            
+
+           
+          
+          
+
+
+            
+
         }
     }
 }
